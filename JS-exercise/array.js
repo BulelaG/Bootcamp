6b00517078b2df5fab7121c469f13e51acd8f5bd
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-03-31 13:42:22 +0200

    Array Methods

---
+++ JS-exercise/array.js
@@ -0,0 +1,19 @@
+
+// Array.methods()
+
+// pop()  for removing the last element
+// push() for adding the last element
+// shift() removes first element
+// unshift() adds first element
+
+
+// let numbers = [1,2,3,4,5,6,7,8]
+//  let a = numbers.pop()
+//  numbers.push(1)
+ 
+//  let b = numbers.shift()
+//  numbers.unshift(8)
+
+
+
+// console.log(numbers)
--- JS-exercise/script2.js
+++ JS-exercise/script2.js
@@ -23,15 +23,26 @@
 // Date object:
 // const date = new Date("2022-03-25");
 
-let numbers = [2,9,11,7,5,3,4,8,15,23,44]
+// 
 
-for(let i = 0; i <= numbers.length ; i++){
+// let i = 0
 
-    if (numbers[i]%2 === 0){
-     console.log(numbers[i])
-    }
+// while( i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+
+
+// let numbers = [2,9,11,7,5,3,4,8,15,23,44]
+
+// for(let i = 0; i < numbers.length ; i++){
+
+//     if (numbers[i]%2 === 0){
+//      console.log(numbers[i])
+//     }
      
-    }
+//     }
 
 
 
@@ -41,11 +52,41 @@ for(let i = 0; i <= numbers.length ; i++){
 //     console.log(i{})
 // }
 
-// number%2 === 0
 
-// let i = 0
 
-// while( i < 10) {
-//     console.log(i)
-//     i++
-// }
+// --------------Get an initial of a an array of names--------------------------------
+
+
+// let fnames = ["Yamnkela","Aphelele","Liya","Bulela"]
+
+// for(let i = 0; i < fnames.length ; i++){
+
+//     {
+//      console.log( fnames[i].charAt(0))     
+
+//     }
+     
+//     }
+
+
+// --------------Get an initial of a an array of names--------------------------------
+
+
+// let fnames = ["Yamnkela","Aphelele","Liya","Bulela"]
+
+// for(let i = 0; i < fnames.length ; i++){
+
+//     {
+//      console.log( fnames[i][0])
+//     }
+     
+//     }
+
+// --------------Get an initial of a name--------------------------------
+
+
+// let a = "Azabenathi".charAt(0)
+
+// console.log(a)
+
+
