c96e73be602bab6e432064e3c1e6257f0c1310a4
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-03-31 16:04:15 +0200

    Array manipulation

--- JS-exercise/array.js
+++ JS-exercise/array.js
@@ -8,7 +8,9 @@
 
 
 // let numbers = [1,2,3,4,5,6,7,8]
+
 //  let a = numbers.pop()
+
 //  numbers.push(1)
  
 //  let b = numbers.shift()
@@ -17,3 +19,29 @@
 
 
 // console.log(numbers)
+
+
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+
+let evenNums = [];
+let  oddNums =[] ;
+
+
+for(let i = 0; i < numbers.length ; i++){
+  
+
+    if (numbers[i]%2 === 0){
+        
+        evenNums.push(numbers[i])
+        }
+        else{
+            oddNums.push(numbers[i])
+        }
+
+   }
+   console.log("Even Numbers")
+
+   console.log(evenNums)
+
+   console.log("Odd Numbers")
+console.log(oddNums)
