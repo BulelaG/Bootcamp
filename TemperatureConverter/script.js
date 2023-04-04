9c30256806103c69deed49c5d4a6cc2ab5e767ce
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-04-04 11:26:39 +0200

    tempconv

---
+++ TemperatureConverter/script.js
@@ -0,0 +1,42 @@
+// function TempConverter(F,C){
+
+// console.log(TempConverter())
+
+
+
+// ------------Fahrenheits to Celcius-----------
+
+// function FahToCel(F){
+// return C = (F-32)*5/9
+
+
+// }
+
+// console.log(FahToCel(1))
+
+
+// ------------Fahrenheits to Celcius-----------
+
+function CelToFah(c){
+
+let F = (c*9/5 )+32 
+console.log(F)
+
+if(F<16){ 
+    return("It's cold, you might want to carry a jacket")
+}
+else if(21=>F && F=>16){
+    return("It is warm, you could wear t-shirt and jeans")
+}
+else (F>21){
+    return("It's a hot day, you can rock your shorts.")
+}
+
+
+}
+console.log(CelToFah(-8))
+
+
+
+
+
