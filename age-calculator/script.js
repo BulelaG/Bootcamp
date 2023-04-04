62a3e9f12dec78528f8e807035a0ec2cecfa032c
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-04-04 15:54:58 +0200

    functions

---
+++ age-calculator/script.js
@@ -0,0 +1,13 @@
+function calcAge(yb){
+
+   let age = 2023 - yb
+
+ if (age<0){console.log(("Age can't be a negative number"))}
+
+  else if( age => 0){console.log(age)}
+
+   else {console.log(("Enter your birth year"))}
+
+
+}
+(calcAge(2024))
---
+++ age-calculator/script2.js
@@ -0,0 +1,21 @@
+function Add10(nmbr){
+
+let results = nmbr + 10
+return results
+
+}
+console.log(Add10(1))
+
+function multi5(nmbr2){
+
+    let answer = nmbr2  * 5
+    return answer
+    
+}
+ console.log(multi5(1))
+
+function sumof2V(v1,v2){
+    let answer  = v1+v2
+    return answer
+}
+console.log(sumof2V(1,5))
