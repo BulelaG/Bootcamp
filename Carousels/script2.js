4c33b57b1284c746095b3c3e0a8894ccab61eb85
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-04-05 17:04:56 +0200

    carousels

--- Carousels/script2.js
+++ Carousels/script2.js
@@ -3,5 +3,22 @@ const slider = carousel.querySelector("ul");
 const slides = carousel.querySelectorAll("li");
 const prevButton = carousel.querySelector(".prev");
 const nextButton = carousel.querySelector(".next")
+let slidePosition = 0;
+const slidesToShow =1;
+const slideWidth = slide[0].getBoundingClientRect().width;
 
+nextButton.addEventListener("click", ()=>{
+    const slidesLeft = slides.length -(slidePosition + slidesToShow);
+    if(slidesLeft > 0){
+        slidePosition += 1;
+        slider.style.transform = 'translateX(-${slidePosition * slideWidth}px)';
+    }
+});
+
+prevButton.addEventListener("click", ()=>{
+    if(slidePosition > 0){
+        slidePosition -= 1;
+        slider.style.transform = 'translateX(-${slidePosition * slideWidth}px)';
+    }
+});
 
