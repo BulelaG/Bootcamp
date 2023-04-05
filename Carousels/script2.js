0ac84ab512b52d54399538ba06a8359fae3288cc
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-04-05 16:44:13 +0200

    carousels

---
+++ Carousels/script2.js
@@ -0,0 +1,7 @@
+const carousel = document.querySelector(".carousel");
+const slider = carousel.querySelector("ul");
+const slides = carousel.querySelectorAll("li");
+const prevButton = carousel.querySelector(".prev");
+const nextButton = carousel.querySelector(".next")
+
+
--- age-calculator/script.js
+++ age-calculator/script.js
@@ -2,12 +2,12 @@ function calcAge(yb){
 
    let age = 2023 - yb
 
- if (age<0){console.log(("Age can't be a negative number"))}
+if(isNaN(yb)) return("Enter your birth year") 
+else if( age >= 0) return age;
+else if (age<0) return("Age can't be a negative number")
 
-  else if( age => 0){console.log(age)}
 
-   else {console.log(("Enter your birth year"))}
 
 
 }
-(calcAge(2024))
+console.log(calcAge(2024))
