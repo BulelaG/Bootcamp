ea569173ffb59cd25f64f9d3de653adb091cc530
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-04-28 11:21:10 +0200

    coppy array to another at position

--- JS-exercise/JS_practice/playground.js
+++ JS-exercise/JS_practice/playground.js
@@ -1,27 +1,43 @@
-let fullnames = [
-   {
-     name:"John",
-     lastname:"Doe"
-    },
-     {
-       name:"Jane",
-     lastname:"Doee"  
-    },
-    {
-      name:"Peter",
-     lastname:"Parker"   
-    }
-    ]
+// LOOPING AN ARRAY OF OBJECTS
 
-    for(l = 0; l <firstnames.length;l++){
-    console.log(firstnames[l])}
+// let fullnames = [
+//    {
+//      name:"John",
+//      lastname:"Doe"
+//     },
+//      {
+//        name:"Jane",
+//      lastname:"Doeee"  
+//     },
+//     {
+//       name:"Peter",
+//      lastname:"Parker"   
+//     }
+//     ]
 
-
+//     for(l = 0; l <firstnames.length;l++){
+//     console.log(firstnames[l])}
     
-    // const firstnames = fullnames.map(person => person.name);
-    // console.log(firstnames)
+//     const firstnames = fullnames.map(person => person.name);
+//     console.log(firstnames)
 
-    // for (let person of fullnames){
-    //     console.log(person.lastname)
-    // }
+//     for (let person of fullnames){
+//         console.log(person.lastname)
+//     }
+
+// ----------------------------------------------------
+
+function copyArray(array1,array2,index){
+
+// arrayCopy=array1.slice() 
+
+array1.splice(index,0,...array2)
+
+console.log(array1)
+console.log(array2)
+
+}
+copyArray([1,2,3],[3,2,1],3)
+
+
 
