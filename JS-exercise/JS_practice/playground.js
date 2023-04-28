e7e57d6c5c2ca7ee0c40f6c298b4a1aa8283ed10
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-04-28 14:58:49 +0200

    reverse string

--- JS-exercise/JS_practice/playground.js
+++ JS-exercise/JS_practice/playground.js
@@ -1,19 +1,9 @@
 // LOOPING AN ARRAY OF OBJECTS
 
 // let fullnames = [
-//    {
-//      name:"John",
-//      lastname:"Doe"
-//     },
-//      {
-//        name:"Jane",
-//      lastname:"Doeee"  
-//     },
-//     {
-//       name:"Peter",
-//      lastname:"Parker"   
-//     }
-//     ]
+//    { name:"John", lastname:"Doe" },
+//    { name:"Jane", lastname:"Doeee" },
+//    {name:"Peter", lastname:"Parker" }]
 
 //     for(l = 0; l <firstnames.length;l++){
 //     console.log(firstnames[l])}
@@ -25,19 +15,29 @@
 //         console.log(person.lastname)
 //     }
 
-// ----------------------------------------------------
+// ---------------Spread oparator-------------------------------------
 
-function copyArray(array1,array2,index){
+// function copyArray(array1,array2,index){
+// arrayCopy=array1.slice()
+// array1.splice(index,0,...array2)
+// console.log(array1)
+// console.log(array2)}
+// copyArray([1,2,3],[3,2,1],1)
 
-// arrayCopy=array1.slice() 
+// -------------Spread oparator-------------------------
+// let ar1 = [1,2,3]
+// let ar2 = [9,11]
+// let n = 1
+// function copy(ar1,ar2,n){
+// return [...ar1.slice(0,n), ...ar2, ...ar1.slice(n)]}
+// copy(ar1,ar2,n)
 
-array1.splice(index,0,...array2)
+// -------------------Reverse a String--------------------
 
-console.log(array1)
-console.log(array2)
-
+let str = "Bulela"
+let c= ""
+for(k = str.length-1; k >= 0; k-- ){
+  c = c + str[k]
+ 
 }
-copyArray([1,2,3],[3,2,1],3)
-
-
-
+console.log(c)
