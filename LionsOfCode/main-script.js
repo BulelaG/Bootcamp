364a551d9b8ab76b3fcb6c0508f5ed6677a02b3b
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-05-02 13:58:15 +0200

    LOC bootstrap

---
+++ LionsOfCode/main-script.js
@@ -0,0 +1,13 @@
+// const name = prompt("What's your name?");
+// const h1 = document.querySelector("name")
+// h1.textContent = name ;
+// h1.style.opacity = 1;
+
+
+function greet(){
+    
+    var name = document.getElementById("search").value
+    alert("Hello"+ " "+name + "!")
+
+
+}
